c2a80c9138ba92b7c884abdd73e38796a696cc57
Roonng <46856430+Roonng@users.noreply.github.com>
2020-01-26 17:51:46 +0800

    2
    轮播图和推荐

--- service/home.js
+++ service/home.js
@@ -2,6 +2,6 @@ import request from './network.js'
 
 export function getMultiData(){
   return request({
-    url: 'http://123.207.32.32:8000/home/multidata'
+    url: '/home/multidata'
   })
 }
--- service/network.js
+++ service/network.js
@@ -1,7 +1,9 @@
+import { baseUrl } from './config.js'
+
 export default function(option){
   return new Promise((resolve, reject) => {
     wx.request({
-      url: option.url,
+      url: baseUrl + option.url,
       method: option.method || 'get',
       data: option.data || {},
       success: resolve,
